bfcb4a71b63966de86cd6bf644a466ec944be9e4
dani08722 <mejias.lodan24@triana.salesianos.edu>
2026-05-11 14:11:32 +0200

    first commit

---
+++ src/main/java/com/salesianostriana/dam/proyectobookversedanielmejias2/ProyectoBookVerseDanielMejias2Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectobookversedanielmejias2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBookVerseDanielMejias2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBookVerseDanielMejias2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/proyectobookversedanielmejias2/ProyectoBookVerseDanielMejias2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectobookversedanielmejias2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBookVerseDanielMejias2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
