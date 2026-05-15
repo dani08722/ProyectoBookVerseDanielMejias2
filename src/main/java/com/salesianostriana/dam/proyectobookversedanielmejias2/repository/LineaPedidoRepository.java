ae3fc559643a2ad8a5dab272d865e538b77ed362
dani08722 <mejias.lodan24@triana.salesianos.edu>
2026-05-15 14:31:01 +0200

    Eliminar implementado

--- src/main/java/com/salesianostriana/dam/proyectobookversedanielmejias2/repository/LineaPedidoRepository.java
+++ src/main/java/com/salesianostriana/dam/proyectobookversedanielmejias2/repository/LineaPedidoRepository.java
@@ -1,9 +1,16 @@
 package com.salesianostriana.dam.proyectobookversedanielmejias2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.salesianostriana.dam.proyectobookversedanielmejias2.models.LineaPedido;
 
 public interface LineaPedidoRepository extends JpaRepository<LineaPedido, Long> {
+
+	@Modifying
+	@Query("delete from LineaPedido lp where lp.libro.isbn = :isbn")
+	void deleteByLibroIsbn(@Param("isbn") String isbn);
 
 }
